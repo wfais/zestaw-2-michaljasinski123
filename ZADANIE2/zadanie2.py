1f713efb6c5f28c46cd3ae703eaf812871be6f8a
Michał Jasiński <michal.1.jasinski@student.uj.edu.pl>
2025-11-10 20:41:26 +0000

    Zadanie 2 skonczone

--- ZADANIE2/zadanie2.py
+++ ZADANIE2/zadanie2.py
@@ -1,9 +1,43 @@
 def rzymskie_na_arabskie(rzymskie):
-    # twój kod
+    arabskie_sl = {                                   
+        "M":1000, "D":500,"C":100,"L":50, "X":10,"V":5, "I":1
+    }
+    if not isinstance(rzymskie, str) or not rzymskie:
+        raise ValueError("Niepoprawna liczba rzymska")
+
+    for x in rzymskie:
+        if x not in arabskie_sl or x in ["IIII", "VV","VVVV", "XXXX", "LL","LLLL", "CCCC", "DD", "MMMM"]:
+            raise ValueError
+        
+    wartosc = 0 
+    ost_cyfra = 0
+    for i in rzymskie[::-1]:
+        liczba = arabskie_sl[i]
+        if liczba>=ost_cyfra:
+            wartosc+=liczba
+            ost_cyfra = liczba
+        else:
+            wartosc-=liczba
+    if rzymskie != arabskie_na_rzymskie(wartosc):
+        raise ValueError 
     return wartosc
 
 def arabskie_na_rzymskie(arabskie):
-    # twój kod
+    rzymskie_sl = {                                   
+        1000: "M",900:"CM", 500: "D",400:"CD", 100: "C",90:"XC",
+        50:"L",40:"XL", 10:"X",9:"IX", 5:"V",4:"IV", 1:"I"
+    }
+    if arabskie>3999 or arabskie<1:
+        raise ValueError("Liczba musi być w zakresie 1-3999")
+    rzymskie = ""
+    reszta = arabskie
+    for i in rzymskie_sl.keys():
+        if reszta>0:
+            mnoznik = i
+            rzymska = rzymskie_sl[i]
+            ilosc = reszta//mnoznik
+            reszta%=mnoznik
+            rzymskie+=rzymska*ilosc
     return rzymskie
 
 if __name__ == '__main__':
