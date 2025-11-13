03df0ff9604ec16f82f97d50f70b5389dcd2c577
Michał Jasiński <michal.1.jasinski@student.uj.edu.pl>
2025-11-13 18:51:05 +0000

    Zadanie 4 skonczone

--- ZADANIE4/zadanie4.py
+++ ZADANIE4/zadanie4.py
@@ -20,7 +20,12 @@ def policz_fragment_pi(pocz: int, kon: int, krok: float, wyniki: list[float], in
     #   - obliczyć lokalną sumę dla przydzielonego przedziału,
     #   - wpisać wynik do wyniki[indeks].
 
-    pass  # zaimplementuj obliczanie fragmentu całki dla danego wątku
+     # zaimplementuj obliczanie fragmentu całki dla danego wątku
+    suma = 0.0
+    for i in range(pocz,kon):
+        x = (i+0.5)*krok
+        suma+= 4.0/(1+x**2)
+    wyniki[indeks] = suma
 
 
 def main():
@@ -43,7 +48,29 @@ def main():
     #   - obliczenie przybliżenia π jako sumy wyników z poszczególnych wątków,
     #   - pomiar czasu i wypisanie przyspieszenia.
     # ---------------------------------------------------------------
-
+    czas_1_watek = 0.0
+    for watki_liczba in LICZBA_WATKOW:
+        t1 = time.perf_counter()
+        watki = []
+        wyniki = [0.0]*watki_liczba
+        liczba_krokow = LICZBA_KROKOW//watki_liczba
+        for j in range(watki_liczba):
+            poczatek = j*liczba_krokow
+            if j == watki_liczba-1:
+                koniec = LICZBA_KROKOW
+            else:
+                koniec = (j+1)*liczba_krokow
+            w = threading.Thread(target=policz_fragment_pi, args=(poczatek, koniec, krok, wyniki, j))
+            watki.append(w)
+            w.start()
+        for w in watki:
+            w.join()
+        pi = sum(wyniki)*krok
+        t2 = time.perf_counter() 
+        czas = t2-t1
+        if watki_liczba == 1:
+            czas_1_watek=czas
+        print(f"Wątki: {watki_liczba}, Czas: {round(czas, 5)} s, Przyspieszenie: {round(czas_1_watek/czas,3)}, Wartość liczby π: {round(pi,14)}")
 
 if __name__ == "__main__":
     main()
